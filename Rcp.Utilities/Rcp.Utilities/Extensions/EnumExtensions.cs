9117dca4be4a9545a529c4061ff4aacc0533bd5b
Jeremy Oursler <11425030+RubberChickenParadise@users.noreply.github.com>
2020-05-20 11:46:58 -0500

    Add EnumExtension.cs
    Add in the Enum Extensions used quite a bit

---
+++ Rcp.Utilities/Rcp.Utilities/Extensions/EnumExtensions.cs
@@ -0,0 +1,52 @@
+using System;
+using System.ComponentModel;
+
+namespace Rcp.Utilities.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription<T>(this T enumerationValue)
+            where T : struct
+        {
+            var type = enumerationValue.GetType();
+            if (!type.IsEnum)
+            {
+                throw new ArgumentException($"{nameof(enumerationValue)} must be of Enum type", nameof(enumerationValue));
+            }
+            var memberInfo = type.GetMember(enumerationValue.ToString());
+            if (memberInfo.Length > 0)
+            {
+                var attrs = memberInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                if (attrs.Length > 0)
+                {
+                    return ((DescriptionAttribute)attrs[0]).Description;
+                }
+            }
+            return enumerationValue.ToString();
+        }
+        
+        public static T GetValueFromDescription<T>(string description)
+        {
+            var type = typeof(T);
+            if (!type.IsEnum) throw new InvalidOperationException();
+            foreach (var field in type.GetFields())
+            {
+                var attribute = Attribute.GetCustomAttribute(field,
+                                                             typeof(DescriptionAttribute)) as DescriptionAttribute;
+                if (attribute != null)
+                {
+                    if (attribute.Description == description)
+                        return (T)field.GetValue(null);
+                }
+                else
+                {
+                    if (field.Name == description)
+                        return (T)field.GetValue(null);
+                }
+            }
+            throw new ArgumentException("Not found.", nameof(description));
+            // or return default(T);
+        }
+    }
+}
