71f7fe892a276d3267fe7c2abb9634588ca5c064
Jeremy Oursler <Jeremy@RubberChickenParadise.com>
2020-05-26 12:34:25 -0500

    Add string chunking

---
+++ Rcp.Utilities/Rcp.Utilities.Tests/StringChunksTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rcp.Utilities.Tests
+{
+    [TestClass]
+    public class StringChunksTests
+    {
+        [TestCategory("String Unit Tests")]
+        [TestMethod]
+        public void StringShorterThanChunk()
+        {
+            var obj = new StringChunks();
+
+            var val = obj.ChunkString("short",
+                                      99)
+                         .ToArray();
+
+            Assert.AreEqual(1,
+                            val.Count());
+            Assert.AreEqual("short",
+                            val[0]);
+        }
+        
+        [TestCategory("String Unit Tests")]
+        [TestMethod]
+        public void StringLongerThanChunk()
+        {
+            var obj = new StringChunks();
+
+            var val = obj.ChunkString("short",
+                                      2)
+                         .ToArray();
+
+            Assert.AreEqual(3,
+                            val.Count());
+            Assert.AreEqual("or",
+                            val[1]);
+        }
+    }
+}
---
+++ Rcp.Utilities/Rcp.Utilities/StringChunks.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.XPath;
+
+namespace Rcp.Utilities
+{
+    public class StringChunks
+    {
+        public IEnumerable<string> ChunkString(string input,
+                                               int    chunkLength)
+        {
+            var retVal = new List<string>((int)Math.Ceiling((decimal)input.Length / (decimal)chunkLength));
+
+            for (int i = 0; i < Math.Ceiling((decimal)input.Length / (decimal)chunkLength); i++)
+            {
+                var multiplier = i;
+                var start = multiplier * chunkLength;
+                var length = input.Length - start > chunkLength ? chunkLength : input.Length - start;
+
+                retVal.Add(input.Substring(start, length));
+            }
+
+            return retVal;
+        }
+    }
+}
